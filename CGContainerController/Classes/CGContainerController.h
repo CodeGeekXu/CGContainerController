475fdd76b8fa76408615b3de035b2f10dfcb5212
CodeGeekXu <codegeekxu@gmail.com>
2018-09-02 10:34:22 +0800

    project created

---
+++ CGContainerController/Classes/CGContainerController.h
@@ -0,0 +1,12 @@
+//
+//  CGContainerController.h
+//  CGContainerController
+//
+//  Created by 徐晨光 on 2018/9/2.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CGContainerController : UIViewController
+
+@end
---
+++ Example/CGContainerController/ContainerController.m
@@ -0,0 +1,37 @@
+//
+//  ContainerController.m
+//  CGContainerController_Example
+//
+//  Created by 徐晨光 on 2018/9/2.
+//  Copyright © 2018年 CodeGeekXu. All rights reserved.
+//
+
+#import "ContainerController.h"
+
+@interface ContainerController ()
+
+@end
+
+@implementation ContainerController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
