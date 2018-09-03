31e29eff0ac1e3936d1a4ebe032a3831af0fa87e
CodeGeekXu <codegeekxu@gmail.com>
2018-09-03 10:47:42 +0800

    示例代码完成

--- Example/CGContainerController/ContainerController.m
+++ Example/CGContainerController/ContainerController.m
@@ -18,29 +18,53 @@
 - (instancetype)init
 {
     CGViewController *page1 = [CGViewController new];
-    page1.view.backgroundColor = [UIColor redColor];
+    page1.view.backgroundColor = [UIColor whiteColor];
     
     CGViewController *page2 = [CGViewController new];
-    page2.view.backgroundColor = [UIColor whiteColor];
+    page2.view.backgroundColor = [UIColor redColor];
     
     CGViewController *page3 = [CGViewController new];
     page3.view.backgroundColor = [UIColor greenColor];
     
-    self = [super initWithTitles:@[@"Page1",@"Page2",@"Page3"] childControllers:@[page1,page2,page3] segmentBarHeight:40];
+    CGViewController *page4 = [CGViewController new];
+    page4.view.backgroundColor = [UIColor blueColor];
+    
+    CGViewController *page5 = [CGViewController new];
+    page5.view.backgroundColor = [UIColor groupTableViewBackgroundColor];
+    
+    CGViewController *page6 = [CGViewController new];
+    page6.view.backgroundColor = [UIColor lightGrayColor];
+    
+    CGViewController *page7 = [CGViewController new];
+    page7.view.backgroundColor = [UIColor purpleColor];
+    
+    self = [super initWithTitles:@[@"Page 1",@"Page 2",@"Page 3",@"Page 4",@"Page 5",@"Page 6",@"Page 7"]
+                childControllers:@[page1,page2,page3,page4,page5,page6,page7]
+                segmentBarHeight:40];
     if (self) {
-        self.segmentBar.indicatorColor = [UIColor blackColor];
-//        self.segmentBar.widthStyle = CGSegmentBarWidthStyleDynamic
-        self.segmentBar.selectedTextAttributes = @{NSFontAttributeName:[UIFont systemFontOfSize:16],NSForegroundColorAttributeName:[UIColor greenColor]};
+        self.segmentBar.indicatorColor = [UIColor blueColor];
+        self.segmentBar.widthStyle = CGSegmentBarWidthStyleDynamic;
+        self.segmentBar.interitemSpacing = 40;
+        self.segmentBar.paddingInsets = UIEdgeInsetsMake(0, 20, 0, 20);
+        self.segmentBar.selectedTextAttributes = @{NSFontAttributeName:[UIFont systemFontOfSize:16],
+                                                   NSForegroundColorAttributeName:[UIColor blueColor]};
+        [self.segmentBar reload];
     }
     return self;
+}
+
+- (void)setupBanner
+{
+    CGFloat width = self.view.bounds.size.width;
+    UIImageView *bannerView = [[UIImageView alloc]initWithFrame:CGRectMake(0, 0, width, width/2)];
+    bannerView.image = [UIImage imageNamed:@"banner"];
+    self.headerView = bannerView;
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    UIView *headView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width, 100)];
-    headView.backgroundColor = [UIColor groupTableViewBackgroundColor];
-    self.headerView = headView;
+    [self setupBanner];
 }
 
 - (void)didReceiveMemoryWarning {
